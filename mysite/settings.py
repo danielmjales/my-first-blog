48bc6a891a75b036b205123abddd91bd7c362dd8
danielmjales <danielmjales@bct.ect.ufrn.br>
2019-01-19 11:10:55 -0300

    bug fix settings.py 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dema$*tr0e)klrqs5^03(jfn_yq$%v#e=qv!vk7o*^fq*nyntp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
