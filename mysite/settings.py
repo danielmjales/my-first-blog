ef767a9258af901239b19b88a4917966da83186b
danielmjales <danielmjales@bct.ect.ufrn.br>
2019-01-19 14:15:21 -0300

    post_list view

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dema$*tr0e)klrqs5^03(jfn_yq$%v#e=qv!vk7o*^fq*nyntp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
